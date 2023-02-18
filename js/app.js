fcb32aaf25290792df16001dc0f7ce17ccd23c09
puqqe _haoireal <kenipham147@gmail.com>
2023-02-19 05:50:03 +0700

    done productDetail

--- js/app.js
+++ js/app.js
@@ -1,24 +1,78 @@
 /** @format */
 
 var app = angular.module("myapp", ["ngRoute"]);
-
 app.config(function ($routeProvider) {
   $routeProvider
-    .when("/homepage", {
-      templateUrl: "homepage.html",
+    .when("/homepage", { templateUrl: "homepage.html" })
+    .when("/menshoes", {
+      templateUrl: "Men-shoes.html?" + Math.random(),
+      controller: "MenshoesCtrl",
     })
-    .when("/about", {
-      templateUrl: "Women-sshoes.html",
+    .when("/womenshoes", {
+      templateUrl: "Women-sshoes.html?" + Math.random(),
+      controller: "WomenshoesCtrl",
     })
-    .otherwise({
-      redirectTo: "/homepage",
+    .when("/productdetail/:productId", {
+      templateUrl: "productsdetail.html?" + Math.random(),
+      controller: "ProductDetailCtrl",
+    })
+    .otherwise({ templateUrl: "homepage.html" });
+});
+
+//! Men-Shoes
+app.controller(
+  "MenshoesCtrl",
+  function ($scope, $rootScope, $routeParams, $http) {
+    $scope.products = [];
+    $http.get("Data/men-products.json").then(function (reponse) {
+      console.log(1);
+      $scope.products = reponse.data;
     });
-});
-
-app.controller("homeCtrl", function ($scope) {
-  // Code for the Home page controller
-});
-
-app.controller("aboutCtrl", function ($scope) {
-  // Code for the About page controller
+  }
+);
+//! Women-Shoes
+app.controller(
+  "WomenshoesCtrl",
+  function ($scope, $rootScope, $routeParams, $http) {
+    $scope.products = [];
+    $http.get("Data/women-products.json").then(function (reponse) {
+      console.log(1);
+      $scope.products = reponse.data;
+    });
+  }
+);
+app.controller(
+  "ProductDetailCtrl",
+  function ($scope, $rootScope, $routeParams, $http) {
+    $scope.products = [];
+    $http.get("Data/men-products.json").then(function (reponse) {
+      $scope.products = $scope.products.concat(reponse.data);
+    });
+    $http.get("Data/women-products.json").then(function (reponse) {
+      $scope.products = $scope.products.concat(reponse.data);
+    
+      $scope.product = $scope.products
+      .filter(function (product) {
+        return product.id === $routeParams.productId.substring(1);
+      })
+      .find(function (product) {
+        return true; // tìm thấy bất kỳ sản phẩm nào trong mảng con
+      });
+    console.log($routeParams.productId.substring(1));
+    console.log($scope.product);
+    });
+    
+  }
+);
+app.controller("SizeController", function ($scope) {
+  $scope.sizes = [
+    "EU 36.5",
+    "EU 37",
+    "EU 38",
+    "EU 38.5",
+    "EU 39",
+    "EU 40",
+    "EU 41",
+    "EU 42",
+  ];
 });
